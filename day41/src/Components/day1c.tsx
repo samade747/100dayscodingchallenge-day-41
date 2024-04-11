4a19d36467809526b4b3b1ea2c9daf192d424b02
Abdul Samad <132301326+samade747@users.noreply.github.com>
2024-04-11 21:18:36 +0500

    918pm 11 04 2024

---
+++ day41/src/Components/day1c.tsx
@@ -0,0 +1,14 @@
+function logUnitVowel(str: string): void { // function that logs the first vowel in a string
+    const vowels = "aeiowAEIOU"; // string of vowels
+    for (const char of str){ // loops through each character in the string
+        if (vowels.includes(char)){ // checks if the character is a vowel
+            console.log(`first vowel found ${char}`); // logs the first vowel found
+            break; // exits the loop if a vowel is found
+    } 
+        console.log(char) // logs the character if it is not a vowel
+
+
+}
+
+logUnitVowel('syzygy')
+
