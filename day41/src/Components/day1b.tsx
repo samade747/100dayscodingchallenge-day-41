b8d9ca7cfcd455dd3e0b3076e55dfc59f2ef77e3
Abdul Samad <132301326+samade747@users.noreply.github.com>
2024-04-11 19:40:08 +0500

    740pm 11 4 2024

---
+++ day41/src/Components/day1b.tsx
@@ -0,0 +1,12 @@
+// intializes the counter at 10
+
+let count: number = 10;
+while(count > 0){
+    if(count === 5){
+        break;    // breaks the loop on 5
+    }
+    console.log(count);
+    count--;    // decrements the counter
+
+} 
+
